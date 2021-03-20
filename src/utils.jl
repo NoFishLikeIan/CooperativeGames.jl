780c16daa00232de1532fab4a76e2cfa18287267
NoFishLikeIan <andreatitton96@gmail.com>
2021-03-20 15:14:25 +0100

    🧹 function definition

--- src/utils.jl
+++ src/utils.jl
@@ -1,3 +1,6 @@
-∑(coll) = isempty(coll) ? 0. : sum(coll)
-
-propersubsets(coll) = (S for S in subsets(coll) if 0 < length(S) < length(coll))
+function ∑(coll)
+    isempty(coll) ? 0. : sum(coll)
+end
+function propersubsets(coll)
+    (S for S in subsets(coll) if 0 < length(S) < length(coll))
+end
