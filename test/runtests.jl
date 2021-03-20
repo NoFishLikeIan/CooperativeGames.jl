a447e8db1a3409e8888c42a0b1499c90c3f74d2a
NoFishLikeIan <andreatitton96@gmail.com>
2021-03-20 14:30:33 +0100

    🐲 test for simple game

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,29 @@
 using CoordinationGames
 using Test
 
-@testset "CoordinationGames.jl" begin
-    # Write your tests here.
+
+@testset "Game" begin
+
+    one, two, three = [1], [2], [3]
+    N = one ∪ two ∪ three
+
+
+    v(S::Int) = v([S])
+    function v(S::CoordinationGames.Players)
+    S = sort(S)
+    if S == N 4
+    elseif S == one 1
+        elseif S == two 2
+        elseif S == three 0
+        elseif S == one ∪ two 3
+        elseif S == one ∪ three 3
+        elseif S == two ∪ three 2
+        elseif isempty(S) 0 end end
+
+    G = CoordinationGames.Game(N, v)
+    coalition = [1, 2, 3]
+    
+    @test G.v(coalition) == 4
+    @test !CoordinationGames.isconvex(G)
+
 end
